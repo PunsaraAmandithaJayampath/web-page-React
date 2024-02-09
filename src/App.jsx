114748484b842001f216068decd96b7127e77646
PunsaraAmandithaJayampath <punsarase@gmail.com>
2024-02-09 14:17:05 +0530

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react'
+import Home from './pages/Home'
+import Animation from './pages/animation'
+import AOS from 'aos';
+import 'aos/dist/aos.js'
+import 'aos/dist/aos.css'
+import Slideshow from './pages/Slideshow';
+
+export default function App() {
+
+  useEffect (()=> {
+    AOS.init()
+  },[])
+
+  return (
+    <div>
+      <Home/>
+      <Slideshow/>
+      <Animation/>
+    </div>
+  )
+}
